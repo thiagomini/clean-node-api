0c2cf4cc03bd0806bc8f4999cff81ec1bb008d1b
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-06 21:11:45 -0300

    test(signup): ensure SignupController returns an error if no password or passwordConfirmation is provided

--- src/presentation/controllers/signup.controller.spec.ts
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -3,7 +3,7 @@ import { HttpStatusCodes } from '../protocols'
 import { SignUpController } from './signup.controller'
 
 describe('SignupController', () => {
-  it('should return 400 if no name is provided', () => {
+  it('should return BAD_REQUEST if no name is provided', () => {
     const sut = new SignUpController()
 
     const httpRequest = {
@@ -18,7 +18,7 @@ describe('SignupController', () => {
     expect(httpResponse?.body).toEqual(new MissingParamException('name'))
   })
 
-  it('should return 400 if no email is provided', () => {
+  it('should return BAD_REQUEST if no email is provided', () => {
     const sut = new SignUpController()
 
     const httpRequest = {
@@ -31,5 +31,35 @@ describe('SignupController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(HttpStatusCodes.BAD_REQUEST)
     expect(httpResponse?.body).toEqual(new MissingParamException('email'))
+  })
+
+  it('should return BAD_REQUEST if no password is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_namy',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(HttpStatusCodes.BAD_REQUEST)
+    expect(httpResponse?.body).toEqual(new MissingParamException('password'))
+  })
+
+  it('should return BAD_REQUEST if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_namy',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(HttpStatusCodes.BAD_REQUEST)
+    expect(httpResponse?.body).toEqual(new MissingParamException('passwordConfirmation'))
   })
 })
