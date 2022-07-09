9d39236dc5aad77d7da858620cf94f7ffff163f2
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-09 15:22:45 -0300

    test(utils): ensure pick function can retrieve multiple key and object pairs

--- src/presentation/utils/object.utils.spec.ts
+++ src/presentation/utils/object.utils.spec.ts
@@ -10,5 +10,14 @@ describe('Object utils', () => {
 
       expect(pick(object, 'key')).toEqual({ key: 'value' })
     })
+
+    it('should pick multiple attributes', () => {
+      const object = {
+        key: 'value',
+        key2: 'value2'
+      }
+
+      expect(pick(object, 'key', 'key2')).toEqual(object)
+    })
   })
 })
