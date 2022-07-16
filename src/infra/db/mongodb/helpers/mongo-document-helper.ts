6266726433606ed6d30f74788f2b5495979f821d
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-16 17:26:50 -0300

    refactor(infra): change return type of addIdToDocument method

--- src/infra/db/mongodb/helpers/mongo-document-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-document-helper.ts
@@ -1,11 +1,7 @@
 import { Document } from 'mongodb'
 import { NullDocumentError } from './errors/null-document.error'
 
-type DocumentWithId = Document & {
-  id: string
-}
-
-export function addIdToDocument<T extends Document> (mongoDocument: T): DocumentWithId {
+export function addIdToDocument<T extends Document> (mongoDocument: T): T & { id: string } {
   if (!mongoDocument) throw new NullDocumentError(mongoDocument)
 
   const { _id, otherProperties } = mongoDocument
