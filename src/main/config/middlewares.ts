73be76e22c974fb837cfe239eaf9795231e51308
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-18 19:04:17 -0300

    feat(main): ensure app uses cors middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
 import type { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser.middleware'
+import { cors } from '../middlewares/cors.middleware'
 export const setupMiddlewaresFor = (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
