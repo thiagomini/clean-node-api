71e2561e37378356f2980f550ab4af11cd053a54
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-18 19:20:57 -0300

    feat(main): ensure app uses content type middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import type { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser.middleware'
+import { contentType } from '../middlewares/content-type.middleware'
 import { cors } from '../middlewares/cors.middleware'
 export const setupMiddlewaresFor = (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
