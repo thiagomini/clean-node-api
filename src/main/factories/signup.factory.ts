4a4ef2dca903e37a15e7647e9bbb4290331fd66b
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-25 19:21:32 -0300

    feat: add LogMongoRepository to SignUp factory

--- src/main/factories/signup.factory.ts
+++ src/main/factories/signup.factory.ts
@@ -3,12 +3,19 @@ import { EmailValidatorAdapter } from '../../utils/email-validator.adapter'
 import { DbAddAccountUseCase } from '../../data/use-cases/add-account/db-add-account.use-case'
 import { BCryptEncrypterAdapter } from '../../infra/cryptography/bcrypt-encrypter.adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account-mongo.repository'
+import { Controller } from '../../presentation/protocols'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log-mongo.repository'
+import { LogDecoratorController } from '../decorators/log.decorator'
 
-export const createSignupController = (): SignUpController => {
+export const createSignupController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const dbAddAccountUseCase = createDbAddAccount()
   const signupController = new SignUpController(emailValidatorAdapter, dbAddAccountUseCase)
-  return signupController
+
+  const logErrorRepository = new LogMongoRepository()
+  const logDecoratorController = new LogDecoratorController(signupController, logErrorRepository)
+
+  return logDecoratorController
 }
 
 const createDbAddAccount = (): DbAddAccountUseCase => {
