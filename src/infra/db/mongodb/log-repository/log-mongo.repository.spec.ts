8c71794d86e08258503e89999e53121e70243cf5
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-25 18:57:52 -0300

    feat(infra): ensure LogMongoRepository creates an error log for a ContextError with a context

--- src/infra/db/mongodb/log-repository/log-mongo.repository.spec.ts
+++ src/infra/db/mongodb/log-repository/log-mongo.repository.spec.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from 'mongodb'
+import { ContextError } from '../../../../errors'
 import { mongoHelper } from '../helpers/mongo-helper'
 import { clearErrorLogsCollection } from '../helpers/test-teardown-helpers'
 import { LogMongoRepository } from './log-mongo.repository'
@@ -30,6 +31,30 @@ describe('LogMongoRepository', () => {
       stack: error.stack,
       cause: null,
       context: {},
+      createdAt: expect.any(Date)
+    })
+  })
+
+  it('should save an error log for ContextError instances with a context', async () => {
+    const logRepository = new LogMongoRepository()
+    const error = new ContextError({
+      message: 'Some error occurred',
+      context: {
+        key: 'value'
+      },
+      errorName: 'CustomName'
+    })
+
+    await logRepository.error(error)
+
+    const errorLog = await (await mongoHelper.getCollection('errors')).findOne()
+
+    expect(errorLog).toEqual({
+      _id: expect.any(ObjectId),
+      name: error.name,
+      stack: error.stack,
+      cause: null,
+      context: error.context,
       createdAt: expect.any(Date)
     })
   })
