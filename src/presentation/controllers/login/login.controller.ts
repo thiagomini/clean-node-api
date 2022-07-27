33a6a0cd7a1a9216bb953a27a097f0ccca08e434
Thiago Martins <thiago_valentim1@hotmail.com>
2022-07-27 18:51:25 -0300

    refactor(presentation): change LoginController params validation to use helper method

--- src/presentation/controllers/login/login.controller.ts
+++ src/presentation/controllers/login/login.controller.ts
@@ -1,22 +1,20 @@
 import { Authentication } from '../../../domain/use-cases/authentication'
 import { InvalidParamException, MissingParamException } from '../../errors'
-import { badRequest, Controller, EmailValidator, HttpResponse, HttpStatusCodes, internalServerError } from '../../protocols'
-import { LoginControllerRequest } from './login.controller.interfaces'
+import { badRequest, Controller, EmailValidator, HttpRequest, HttpResponse, HttpStatusCodes, internalServerError } from '../../protocols'
+import { firstMissingAttributeOf } from '../../utils'
 
 export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator, private readonly authentication: Authentication) { }
 
-  async handle (httpRequest: LoginControllerRequest): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { email, password } = httpRequest.body ?? {}
+      const missingAttribute = firstMissingAttributeOf(httpRequest.body, ['email', 'password'])
 
-      if (!email) {
-        return badRequest(new MissingParamException('email'))
+      if (missingAttribute) {
+        return badRequest(new MissingParamException(missingAttribute))
       }
 
-      if (!password) {
-        return badRequest(new MissingParamException('password'))
-      }
+      const { email, password } = httpRequest.body
 
       const emailIsValid = this.emailValidator.isValid(email)
 
