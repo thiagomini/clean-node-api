8a7f54167633d2556178a67c49ca4969d6153c64
Thiago Martins <thiago_valentim1@hotmail.com>
2022-08-01 18:35:08 -0300

    test(presentation): ensure RequiredFieldValidation returns undefined if required field is set

--- src/presentation/validators/required-field.validation.spec.ts
+++ src/presentation/validators/required-field.validation.spec.ts
@@ -11,5 +11,14 @@ describe('RequiredFieldValidation', () => {
 
       expect(sut.validate(input)).toEqual(new MissingParamException('requiredFieldName'))
     })
+
+    it('should return undefined if required field is set', () => {
+      const input = {
+        requiredFieldName: 'value'
+      }
+      const sut = new RequiredFieldValidation(['requiredFieldName'])
+
+      expect(sut.validate(input)).toBeUndefined()
+    })
   })
 })
