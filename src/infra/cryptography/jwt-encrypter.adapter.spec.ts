24f9ab434f157efaaaf5171d757f5a751048b54b
Thiago Martins <thiago_valentim1@hotmail.com>
2022-08-10 20:18:44 -0300

    feat(infra): ensure JwtEncrypterAdapter returns encrypted_id on success

--- src/infra/cryptography/jwt-encrypter.adapter.spec.ts
+++ src/infra/cryptography/jwt-encrypter.adapter.spec.ts
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken'
 import { JwtEcnrypterAdapter } from './jwt-encrypter.adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): string => 'encrypted_id'
+}))
+
 describe('JwtEcnrypterAdapter', () => {
   describe('encrypt', () => {
     it('should call jsonwebtoken.sign with correct values', async () => {
@@ -10,6 +14,14 @@ describe('JwtEcnrypterAdapter', () => {
       await sut.encrypt('any_id')
 
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+
+    it('should return encrypted_id on sign success', async () => {
+      const sut = new JwtEcnrypterAdapter('secret')
+
+      const accessToken = await sut.encrypt('any_id')
+
+      expect(accessToken).toBe('encrypted_id')
     })
   })
 })
