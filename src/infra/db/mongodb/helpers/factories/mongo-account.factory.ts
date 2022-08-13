1dbcc9d711538a3f6d9c978f35c4eaff0335435a
Thiago Martins <thiago_valentim1@hotmail.com>
2022-08-13 10:10:48 -0300

    refactor(infra): refactor MongoAccountFactory.createAccount to increase readability

--- src/infra/db/mongodb/helpers/factories/mongo-account.factory.ts
+++ src/infra/db/mongodb/helpers/factories/mongo-account.factory.ts
@@ -1,4 +1,4 @@
-import { Collection, ObjectId } from 'mongodb'
+import { Collection, ObjectId, OptionalId } from 'mongodb'
 import { AccountModel } from '../../../../../domain/models'
 import { ModelAttributes } from '../../../../../domain/models/model-attributes'
 import { addIdToDocument } from '../mongo-document-helper'
@@ -8,19 +8,28 @@ export class MongoAccountFactory {
   constructor (private readonly accountCollection: Collection<ModelAttributes<AccountModel>>) {}
 
   public async createAccount (accountInput: Partial<AccountModel> = {}): Promise<AccountModel> {
-    const idToBeInserted = new ObjectId(accountInput.id) ?? new ObjectId()
-    const { id, ...dataWithoutId } = accountInput
+    const finalInput = this.mergeGivenAndDefaultAttributes(accountInput)
+    await this.accountCollection.insertOne(finalInput)
+    return addIdToDocument(finalInput) as AccountModel
+  }
 
-    const finalInput = Object.assign({}, {
-      _id: idToBeInserted,
+  private mergeGivenAndDefaultAttributes (accountInput: Partial<AccountModel> = {}): OptionalId<ModelAttributes<AccountModel>> {
+    const mongoIdToBeInserted = new ObjectId(accountInput.id)
+    const { id, ...accountInputWithoutId } = accountInput
+
+    return Object.assign({}, {
+      _id: mongoIdToBeInserted,
+      ...this.getAccountDefaultAttributes()
+    }, accountInputWithoutId)
+  }
+
+  private getAccountDefaultAttributes (): ModelAttributes<AccountModel> {
+    return {
       name: 'valid_name',
       email: 'valid_email',
       accessToken: 'valid_access_token',
       password: 'hashed_password'
-    }, dataWithoutId)
-
-    await this.accountCollection.insertOne(finalInput)
-    return addIdToDocument(finalInput) as AccountModel
+    }
   }
 
   static async createFactory (): Promise<MongoAccountFactory> {
