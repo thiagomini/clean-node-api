524b3f040e168d6f61911fe52735a8b8ad55580f
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-11 11:05:12 -0300

    feat(utils): ensure getFirstDefinedResponse returns only the first response value when passed function returns defined value for two elements

--- src/utils/array-utils.spec.ts
+++ src/utils/array-utils.spec.ts
@@ -32,5 +32,24 @@ describe('array utils', () => {
       // Assert
       expect(response).toBeUndefined()
     })
+
+    describe('when the function returns a defined value for two elements', () => {
+      it('should return only the first response value', () => {
+        // Arrange
+        const returnsIfIsNumber = (element: unknown): Optional<number> => {
+          if (Number.isInteger(element)) {
+            return element as number
+          }
+        }
+
+        const testArray = [true, {}, 1, 'string', 42]
+
+        // Act
+        const response = getFirstDefinedResponse(testArray, returnsIfIsNumber)
+
+        // Assert
+        expect(response).toBe(1)
+      })
+    })
   })
 })
--- src/utils/array-utils.ts
+++ src/utils/array-utils.ts
@@ -1,10 +1,10 @@
 import { Optional } from './utility-types'
 
 export function getFirstDefinedResponse<T extends (args: any) => unknown > (array: any[], functionToApply: Function): Optional<ReturnType<T>> {
-  const maybeUndefined = array.reduce((_, inputToValidate) => {
-    const response = functionToApply(inputToValidate)
-    return response
-  }, undefined)
-
-  return maybeUndefined
+  for (const element of array) {
+    const response = functionToApply(element)
+    if (response !== undefined) {
+      return response
+    }
+  }
 }
