1fcbedad877b0184248bd0cae68cba541ccfa07e
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-11 10:43:13 -0300

    refactor(validation): extract validation of each input to private method

--- src/validation/validators/each-validation.ts
+++ src/validation/validators/each-validation.ts
@@ -4,15 +4,21 @@ import { Optional } from '../../utils'
 export class EachValidation implements Validation {
   constructor (private readonly validators: Validation[]) {}
 
-  validate (input: any[]): Optional<Error> {
+  validate (inputs: any[]): Optional<Error> {
     for (const validation of this.validators) {
-      const errorOrUndefined = input.reduce((_, inputToValidate) => {
-        const maybeError = validation.validate(inputToValidate)
-        return maybeError
-      }, undefined)
+      const errorOrUndefined = this.validateEachInputWithValidation(inputs, validation)
       if (errorOrUndefined) {
         return errorOrUndefined
       }
     }
+  }
+
+  private validateEachInputWithValidation (inputs: any[], validation: Validation): Optional<Error> {
+    const errorOrUndefined = inputs.reduce((_, inputToValidate) => {
+      const maybeError = validation.validate(inputToValidate)
+      return maybeError
+    }, undefined)
+
+    return errorOrUndefined
   }
 }
