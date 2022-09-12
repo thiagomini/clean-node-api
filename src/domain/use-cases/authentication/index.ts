018cc8cbf0c1f6aafa83ecf78337cf0a61c94a61
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-12 20:01:12 -0300

    refactor(presentation): extract sut creation to utility function

--- src/domain/use-cases/authentication/index.ts
+++ src/domain/use-cases/authentication/index.ts
@@ -1,2 +1,3 @@
 export * from './authentication.use-case'
 export * from './authentication.input'
+export * from './load-account-by-token.use-case'
--- src/presentation/middlewares/auth.middleware.spec.ts
+++ src/presentation/middlewares/auth.middleware.spec.ts
@@ -3,56 +3,68 @@ import { AuthMiddleware } from './auth.middleware'
 import { HttpRequest } from '../protocols'
 import { forbidden } from '../utils/http-responses-factories'
 import { AccountModel } from '../../domain/models'
+import { LoadAccountByTokenUseCase } from '../../domain/use-cases/authentication'
 
 describe('AuthMiddleware', () => {
   describe('handle', () => {
     it('should return 403 if no x-access-token header is provided', async () => {
-      class LoadAccountByTokenStub {
-        async load (): Promise<AccountModel> {
-          return {
-            id: 'any_id',
-            email: 'any_email',
-            name: 'any_name',
-            password: 'any_password',
-            accessToken: 'any_access_token'
-          }
-        }
-      }
-      const loadAccountByTokenStub = new LoadAccountByTokenStub()
-      const sut = new AuthMiddleware(loadAccountByTokenStub)
-      const httpRequest: HttpRequest = {
+      // Arrange
+      const { sut } = createSut()
+      const httpRequest: HttpRequest = { }
 
-      }
+      // Act
       const response = await sut.handle(httpRequest)
 
+      // Assert
       expect(response).toEqual(forbidden(new AccessDeniedException()))
     })
 
     it('should call loadAccountByToken with correct accessToken', async () => {
-      class LoadAccountByTokenStub {
-        async load (): Promise<AccountModel> {
-          return {
-            id: 'any_id',
-            email: 'any_email',
-            name: 'any_name',
-            password: 'any_password',
-            accessToken: 'any_access_token'
-          }
-        }
-      }
-      const loadAccountByTokenStub = new LoadAccountByTokenStub()
-
-      const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
-
-      const sut = new AuthMiddleware(loadAccountByTokenStub)
+      // Arrange
+      const { sut, loadAccountByTokenStub } = createSut()
       const httpRequest: HttpRequest = {
         headers: {
           'x-access-token': 'any_token'
         }
       }
+      const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+
+      // Act
       await sut.handle(httpRequest)
 
+      // Assert
       expect(loadSpy).toHaveBeenCalledWith('any_token')
     })
   })
 })
+
+interface SutFactoryResponse {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByTokenUseCase
+}
+
+const createSut = (): SutFactoryResponse => {
+  const loadAccountByTokenStub = createLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return {
+    loadAccountByTokenStub,
+    sut
+  }
+}
+
+const createLoadAccountByTokenStub = (): LoadAccountByTokenUseCase => {
+  class LoadAccountByTokenStub implements LoadAccountByTokenUseCase {
+    async load (): Promise<AccountModel> {
+      return {
+        id: 'any_id',
+        email: 'any_email',
+        name: 'any_name',
+        password: 'any_password',
+        accessToken: 'any_access_token'
+      }
+    }
+  }
+  const loadAccountByTokenStub = new LoadAccountByTokenStub()
+  return loadAccountByTokenStub
+}
