ea666b6286ffef44527cf0273905aae094ec134c
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-12 19:54:38 -0300

    feat(presentation): ensure auth middleware calls loadAccountByTokenUseCase with correct token

--- src/presentation/middlewares/auth.middleware.spec.ts
+++ src/presentation/middlewares/auth.middleware.spec.ts
@@ -2,16 +2,57 @@ import { AccessDeniedException } from '../errors'
 import { AuthMiddleware } from './auth.middleware'
 import { HttpRequest } from '../protocols'
 import { forbidden } from '../utils/http-responses-factories'
+import { AccountModel } from '../../domain/models'
+
 describe('AuthMiddleware', () => {
   describe('handle', () => {
     it('should return 403 if no x-access-token header is provided', async () => {
-      const sut = new AuthMiddleware()
+      class LoadAccountByTokenStub {
+        async load (): Promise<AccountModel> {
+          return {
+            id: 'any_id',
+            email: 'any_email',
+            name: 'any_name',
+            password: 'any_password',
+            accessToken: 'any_access_token'
+          }
+        }
+      }
+      const loadAccountByTokenStub = new LoadAccountByTokenStub()
+      const sut = new AuthMiddleware(loadAccountByTokenStub)
       const httpRequest: HttpRequest = {
 
       }
       const response = await sut.handle(httpRequest)
 
       expect(response).toEqual(forbidden(new AccessDeniedException()))
+    })
+
+    it('should call loadAccountByToken with correct accessToken', async () => {
+      class LoadAccountByTokenStub {
+        async load (): Promise<AccountModel> {
+          return {
+            id: 'any_id',
+            email: 'any_email',
+            name: 'any_name',
+            password: 'any_password',
+            accessToken: 'any_access_token'
+          }
+        }
+      }
+      const loadAccountByTokenStub = new LoadAccountByTokenStub()
+
+      const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+
+      const sut = new AuthMiddleware(loadAccountByTokenStub)
+      const httpRequest: HttpRequest = {
+        headers: {
+          'x-access-token': 'any_token'
+        }
+      }
+      await sut.handle(httpRequest)
+
+      expect(loadSpy).toHaveBeenCalledWith('any_token')
     })
   })
 })
--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -3,7 +3,12 @@ import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 import { forbidden } from '../utils/http-responses-factories'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadAccountByTokenUseCase: any) {
+
+  }
+
   async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
+    await this.loadAccountByTokenUseCase.load(httpRequest?.headers?.['x-access-token'])
     return forbidden(new AccessDeniedException())
   }
 }
