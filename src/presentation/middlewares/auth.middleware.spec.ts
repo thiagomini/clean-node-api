0858ffca32abb21a97722f4e2c3cbc77c1dbaeeb
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-15 19:18:59 -0300

    feat(presentation): ensure auth middleware returns 200 with account id when token is valid and user exists

--- src/presentation/middlewares/auth.middleware.spec.ts
+++ src/presentation/middlewares/auth.middleware.spec.ts
@@ -1,7 +1,7 @@
 import { AccessDeniedException } from '../errors'
 import { AuthMiddleware } from './auth.middleware'
 import { HttpRequest } from '../protocols'
-import { forbidden } from '../utils/http-responses-factories'
+import { forbidden, ok } from '../utils/http-responses-factories'
 import { AccountModel } from '../../domain/models'
 import { LoadAccountByTokenUseCase } from '../../domain/use-cases/authentication'
 import { Optional } from '../../utils'
@@ -45,6 +45,21 @@ describe('AuthMiddleware', () => {
       // Assert
       expect(response).toEqual(forbidden(new AccessDeniedException()))
     })
+
+    it('should return 200 with account id if token is valid and user exists', async () => {
+      // Arrange
+      const { sut } = createSut()
+      const httpRequest = createFakeRequest()
+
+      // Act
+      const response = await sut.handle(httpRequest)
+
+      // Assert
+      const accountId = getAccountModel().id
+      expect(response).toEqual(ok({
+        accountId
+      }))
+    })
   })
 })
 
@@ -66,18 +81,20 @@ const createSut = (): SutFactoryResponse => {
 const createLoadAccountByTokenStub = (): LoadAccountByTokenUseCase => {
   class LoadAccountByTokenStub implements LoadAccountByTokenUseCase {
     async load (): Promise<Optional<AccountModel>> {
-      return {
-        id: 'any_id',
-        email: 'any_email',
-        name: 'any_name',
-        password: 'any_password',
-        accessToken: 'any_access_token'
-      }
+      return getAccountModel()
     }
   }
   const loadAccountByTokenStub = new LoadAccountByTokenStub()
   return loadAccountByTokenStub
 }
+
+const getAccountModel = (): AccountModel => ({
+  id: 'any_id',
+  email: 'any_email',
+  name: 'any_name',
+  password: 'any_password',
+  accessToken: 'any_access_token'
+})
 
 const createFakeRequest = (): HttpRequest => ({
   headers: {
--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -1,14 +1,25 @@
+import { LoadAccountByTokenUseCase } from '../../domain/use-cases/authentication'
 import { AccessDeniedException } from '../errors'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
-import { forbidden } from '../utils/http-responses-factories'
+import { forbidden, ok } from '../utils/http-responses-factories'
 
 export class AuthMiddleware implements Middleware {
-  constructor (private readonly loadAccountByTokenUseCase: any) {
+  constructor (private readonly loadAccountByTokenUseCase: LoadAccountByTokenUseCase) {
 
   }
 
   async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
-    await this.loadAccountByTokenUseCase.load(httpRequest?.headers?.['x-access-token'])
+    const accessToken = httpRequest?.headers?.['x-access-token']
+
+    if (accessToken) {
+      const existingUser = await this.loadAccountByTokenUseCase.load(httpRequest?.headers?.['x-access-token'])
+      if (existingUser) {
+        return ok({
+          accountId: existingUser.id
+        })
+      }
+    }
+
     return forbidden(new AccessDeniedException())
   }
 }
