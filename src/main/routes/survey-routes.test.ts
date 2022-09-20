582c827866f4c1b16a3e2d7d044d2e8ae029ca06
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-20 20:22:29 -0300

    feat(main): ensure add survey route returns 403 when user is not an Admin

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,13 +1,28 @@
 import request from 'supertest'
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
-import { clearSurveysCollection } from '../../infra/db/mongodb/helpers/test-teardown-helpers'
+import {
+  clearAccountsCollection,
+  clearSurveysCollection,
+} from '../../infra/db/mongodb/helpers/test-teardown-helpers'
 import { HttpStatusCodes } from '../../presentation/protocols'
 import app from '../config/app'
 import { AUTH_HEADER } from '../../presentation/middlewares/auth-header-key'
+import { Collection } from 'mongodb'
+import { getAccountsCollection } from '../../infra/db/mongodb/helpers/collections'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+import { Role } from '../../auth'
 
 describe('survey routes', () => {
+  let accountsCollection: Collection
+
+  beforeAll(async () => {
+    accountsCollection = await getAccountsCollection()
+  })
+
   afterAll(async () => {
     await clearSurveysCollection()
+    await clearAccountsCollection()
     await mongoHelper.disconnect()
   })
 
@@ -34,6 +49,43 @@ describe('survey routes', () => {
       await request(app)
         .post('/api/surveys')
         .set(AUTH_HEADER, 'invalid_token')
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              image: 'https://loremflickr.com/640/480/cats',
+              answer: 'any_answer',
+            },
+            {
+              answer: 'any_answer_2',
+            },
+          ],
+        })
+        .expect(HttpStatusCodes.FORBIDDEN)
+    })
+
+    it('should return 403 when user is not an Admin', async () => {
+      const user = await accountsCollection.insertOne({
+        name: 'Thiago',
+        email: 'thiago@mail.com',
+        password: 'password',
+        role: Role.User,
+      })
+      const accessToken = sign(user.insertedId.toString(), env.jwtSecret)
+      await accountsCollection.updateOne(
+        {
+          _id: user.insertedId,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      )
+
+      await request(app)
+        .post('/api/surveys')
+        .set(AUTH_HEADER, accessToken)
         .send({
           question: 'any_question',
           answers: [
