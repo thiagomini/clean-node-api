f3cf094bf2d0e87b9855757f5e90b345f045b0ec
Thiago Martins <thiago_valentim1@hotmail.com>
2022-10-01 14:50:44 -0300

    feat(domain): add find survey by id use case

---
+++ src/domain/use-cases/find-survey/find-survey-by-id.use-case.ts
@@ -0,0 +1,5 @@
+import { SurveyModel } from '../../models'
+
+export interface FindSurveyByIdUseCase {
+  findById(id: string): Promise<SurveyModel>
+}
---
+++ src/domain/use-cases/find-survey/index.ts
@@ -0,0 +1,1 @@
+export * from './find-survey-by-id.use-case'
