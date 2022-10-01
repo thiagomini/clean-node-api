9f88d5f595a14936179b2565f3ab38cc34457872
Thiago Martins <thiago_valentim1@hotmail.com>
2022-10-01 12:02:45 -0300

    refactor(main): replace user manual creation by AuthDSL usage

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,26 +1,17 @@
-import request from 'supertest'
+import { Role } from '@/auth'
 import { mongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import {
   clearAccountsCollection,
   clearSurveysCollection,
 } from '@/infra/db/mongodb/helpers/test-teardown-helpers'
-import { HttpStatusCodes } from '@/presentation/protocols'
-import app from '../config/app'
 import { AUTH_HEADER } from '@/presentation/middlewares/auth-header-key'
-import { Collection } from 'mongodb'
-import { getAccountsCollection } from '@/infra/db/mongodb/helpers/collections'
-import { sign } from 'jsonwebtoken'
-import env from '../config/env'
-import { Role } from '@/auth'
+import { HttpStatusCodes } from '@/presentation/protocols'
+import request from 'supertest'
+import app from '../config/app'
 import { AuthDSL } from '../dsl/auth/auth.dsl'
 
 describe('survey routes', () => {
-  let accountsCollection: Collection
   const authDSL = AuthDSL.create()
-
-  beforeAll(async () => {
-    accountsCollection = await getAccountsCollection()
-  })
 
   afterAll(async () => {
     await clearSurveysCollection()
@@ -67,27 +58,13 @@ describe('survey routes', () => {
     })
 
     it('should return 403 when user is not an Admin', async () => {
-      const user = await accountsCollection.insertOne({
-        name: 'Thiago',
-        email: 'thiago@mail.com',
-        password: 'password',
+      const { accessToken } = await authDSL.signupUser({
         role: Role.User,
       })
-      const accessToken = sign(user.insertedId.toString(), env.jwtSecret)
-      await accountsCollection.updateOne(
-        {
-          _id: user.insertedId,
-        },
-        {
-          $set: {
-            accessToken,
-          },
-        }
-      )
 
       await request(app)
         .post('/api/surveys')
-        .set(AUTH_HEADER, accessToken)
+        .set(AUTH_HEADER, accessToken as string)
         .send({
           question: 'any_question',
           answers: [
