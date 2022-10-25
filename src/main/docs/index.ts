1fe4ab961c3add26d7348665c1fd912c457d4d93
Thiago Martins <thiago_valentim1@hotmail.com>
2022-10-25 18:54:26 -0300

    docs: add swagger api

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,1 +1,9 @@
-export default {}
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description:
+      'API do curso do Mango para realizar enquetes entre programadores',
+    version: '1.0.0',
+  },
+}
