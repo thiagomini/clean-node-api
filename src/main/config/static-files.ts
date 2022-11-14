5664f34def18f2ac1d5aecdf229423479d0dce2f
Thiago Martins <thiago_valentim1@hotmail.com>
2022-11-14 19:12:33 -0300

    chore(main): remove console.log

--- src/main/config/static-files.ts
+++ src/main/config/static-files.ts
@@ -3,6 +3,5 @@ import { resolve } from 'path'
 
 export const setupStaticFilesFor = (app: Express): void => {
   const staticFilesPath = resolve(__dirname, '../../static')
-  console.log(staticFilesPath)
   app.use('/static', express.static(resolve(staticFilesPath)))
 }
