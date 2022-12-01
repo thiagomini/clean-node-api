969afe4124831131ec699ba72a0ec094f522dfc9
Thiago Martins <thiago_valentim1@hotmail.com>
2022-12-01 18:49:27 -0300

    test(graphql): add login failure test case

--- tests/main/graphql/auth/login.e2e.test.ts
+++ tests/main/graphql/auth/login.e2e.test.ts
@@ -45,4 +45,32 @@ describe('login e2e', () => {
       },
     })
   })
+
+  it('should return an error informing the password is wrong', async () => {
+    const existingUser = await authDSL.signupUser({
+      password: '123',
+    })
+
+    const queryData = queryBuilder.loginUser({
+      email: existingUser.email,
+      password: 'wrong-password',
+    })
+
+    const response = await request(app)
+      .post('/graphql')
+      .send(queryData)
+      .expect(HttpStatusCodes.OK)
+
+    expect(response.body.errors).toMatchObject([
+      {
+        extensions: {
+          exception: {
+            name: 'UnauthorizedError',
+          },
+        },
+        message: 'Unauthorized access requested',
+      },
+    ])
+    expect(response.body.data).toBeNull()
+  })
 })
